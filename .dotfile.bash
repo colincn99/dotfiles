59a5b2659456fd1973decc0eab41202395794dd4
colincn99 <colinchun99@gmail.com>
2025-08-07 14:51:47 -0700

    Swapped menu-complete shortcuts

--- .dotfile.bash
+++ .dotfile.bash
@@ -16,8 +16,8 @@ fi
 
 # Bash completion
 bind -s 'set completion-ignore-case on'
-bind '\C-f: menu-complete'
-bind '"\e[Z": menu-complete-backward'
+bind '"\e[Z": menu-complete'
+bind '\C-f: menu-complete-backward'
 bind 'set show-all-if-ambiguous on'
 bind 'set show-all-if-unmodified on'
 
